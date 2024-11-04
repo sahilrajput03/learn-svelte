8615eda1528d7090cc5dbbe9f7da87bce1d74462
Sahil Rajput <sahilrajput03@gmail.com>
2024-11-04 14:14:25 +0530

    Add source for openinvscode.

--- my-app-s5/src/routes/OpenFileInVscode.svelte
+++ my-app-s5/src/routes/OpenFileInVscode.svelte
@@ -10,11 +10,12 @@
 {#if dev}
 	<a
 		class="absolute bottom-0 mb-2 text-blue-400"
-		href={`vscode://file${projectPath.concat(relativeFilePath)}`}>Open file in VS Code</a
+		href={`vscode://file${projectPath.concat(relativeFilePath)}:1`}>Open file in VS Code</a
 	>
 {/if}
 
 <!-- [Tested on MacOS] Below code works to open a vscode file from directly browser. -->
+<!-- SOURCE: https://stackoverflow.com/a/53786004/10012446 -->
 <!-- <a
 	href="vscode://file/Users/apple/Documents/github_repos/learn-svelte/my-app-s5/src/routes/Group1.svelte"
 	>Open file</a
