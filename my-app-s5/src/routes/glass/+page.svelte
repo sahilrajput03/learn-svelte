f607ca8d34059b021a722a70e58b48951de9f714
Sahil Rajput <sahilrajput03@gmail.com>
2024-11-16 05:19:50 +0530

    Add glass page.

---
+++ my-app-s5/src/routes/glass/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+	const imgs = [
+		'1.jpg',
+		'2.jpg',
+		'3.jpg',
+		'4.jpg',
+		'5.jpg',
+		'6.jpg',
+		'7.jpg',
+		'8.webp',
+		'9.jpg',
+		'10jpeg',
+		'11.jpg'
+	];
+	let index = $state(0);
+	let src = $derived(imgs[index]);
+
+	$effect(() => {
+		let id = setInterval(() => {
+			if (index === imgs.length - 1) {
+				index = 0;
+			} else {
+				index++;
+			}
+		}, 7_000);
+		return () => clearInterval(id);
+	});
+</script>
+
+<img style="margin: auto;" src={`/glass-imgs/${src}`} alt="glass" />
---
+++ my-app-s5/src/routes/test3/+page.svelte
@@ -0,0 +1,7 @@
+<!-- SvelteKit Tutorial-8: https://svelte.dev/tutorial/kit/lib -->
+<script>
+	import { message } from '$lib/message';
+</script>
+
+<h1>home</h1>
+<p>{message}</p>
