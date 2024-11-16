c4ff63e1cb1fef2b25ee35e105e027f637b71472
Sahil Rajput <sahilrajput03@gmail.com>
2024-11-16 20:31:34 +0530

    updat.

--- my-app-s5/src/routes/thoughts/+page.svelte
+++ my-app-s5/src/routes/thoughts/+page.svelte
@@ -3,34 +3,40 @@
 
 	let thoughts = $state<string[]>([]);
 	let thoughtsToShow = $state(['t1', 't2']);
-	let showSahilThoughtsInitially = true;
-	let showSahilThoughts = $state(showSahilThoughtsInitially);
+	let isShowSahilThoughtsInitially = true;
+	let isShowSahilThoughts = $state(isShowSahilThoughtsInitially);
 
-	const load = async () => {
+	const showSahilThoughts = () => {
+		console.log('triggered');
+		thoughtsToShow = thoughts.filter((thought) => thought.includes('~ Sahil'));
+		// To remove `~ Sahil` use below code instead
+		// thoughts = text
+		// 	.split('\n')
+		// 	.filter((thought) => thought.includes('~ Sahil'))
+		// 	.map((thought) => thought.replace('~ Sahil', '').trim());
+	};
+
+	onMount(async () => {
+		console.log('load-fn');
+
 		const response = await fetch(
 			'https://raw.githubusercontent.com/sahilrajput03/sahilrajput03/refs/heads/main/thoughts-principles-react/src/thoughts.md'
 		);
 		const text = await response.text();
 		thoughts = text.split('\n');
 
-		if (showSahilThoughtsInitially) {
-			thoughtsToShow = thoughts.filter((thought) => thought.includes('~ Sahil'));
-
-			// To remove ~ Sahil from each thought
-			// thoughts = text
-			// 	.split('\n')
-			// 	.filter((thought) => thought.includes('~ Sahil'))
-			// 	.map((thought) => thought.replace('~ Sahil', '').trim());
+		if (isShowSahilThoughtsInitially) {
+			showSahilThoughts();
 		} else {
 			thoughtsToShow = thoughts;
 		}
-	};
-
-	onMount(load);
+	});
 
 	$effect(() => {
-		if (showSahilThoughts) {
-			thoughtsToShow = thoughts.filter((thought) => thought.includes('~ Sahil'));
+		console.log('effect-fn');
+
+		if (isShowSahilThoughts) {
+			showSahilThoughts();
 		} else {
 			thoughtsToShow = thoughts;
 		}
@@ -39,7 +45,7 @@
 
 <section>
 	<h1 class="text-2xl">Thoughts & Principles</h1>
-	<input type="checkbox" bind:checked={showSahilThoughts} />
+	<input type="checkbox" bind:checked={isShowSahilThoughts} />
 
 	<ol>
 		{#each thoughtsToShow as thought}
