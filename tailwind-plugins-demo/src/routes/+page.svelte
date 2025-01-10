6d1b3d636231e1c65481aaa81bac21db43fbe1a0
Sahil Rajput <sahilrajput03@gmail.com>
2025-01-10 18:27:51 +0530

    Update.

--- tailwind-plugins-demo/src/routes/+page.svelte
+++ tailwind-plugins-demo/src/routes/+page.svelte
@@ -1,3 +1,9 @@
+<script>
+	import Forms1 from './Forms1.svelte';
+	import Forms2 from './Forms2.svelte';
+	import Typography from './Typography.svelte';
+</script>
+
 <h1 class="mb-10 text-2xl font-bold">Tailwind Plugins Demo by Sahil Rajput</h1>
 <!-- Typography Docs: https://github.com/tailwindlabs/tailwindcss-typography -->
 
@@ -13,67 +19,9 @@
 	<li><code>@tailwindcss/container-queries</code></li>
 </ul>
 
-<div class="my-5 border-2 border-purple-600 p-3">
-	<h2 class="mb-3 text-2xl font-bold">Typography Example</h2>
-	<!-- / // & Adding class `prose` to a html element applies tailwin-typography plugin to vanilla html inside it -->
-	<div class="prose mt-3">
-		<h1>Garlic bread with cheese: What the science tells us (h1)</h1>
-		<p>
-			For years parents have espoused the health benefits of eating garlic bread with cheese to
-			their children, with the food earning such an iconic status in our culture that kids will
-			often dress up as warm, cheesy loaf for Halloween.
-		</p>
-		<p>
-			But a recent study shows that the celebrated appetizer may be linked to a series of rabies
-			cases springing up around the country.
-		</p>
+<h2 class="mb-[100px] mt-[100px] text-8xl font-bold">Tailwindcss Typography</h2>
+<Typography />
 
-		<h2>Ordered, Unordered and Description Lists (h2)</h2>
-		<p>Examples of ordered, unordered lists and description lists.</p>
-
-		<h3>Ordered List (h3)</h3>
-		<ol>
-			<li>This is list item 1</li>
-			<li>This is list item 2</li>
-			<li>This is list item 3</li>
-		</ol>
-
-		<h3>Unordered List (h3)</h3>
-		<ul>
-			<li>This is list item 1</li>
-			<li>This is list item 2</li>
-			<li>This is list item 3</li>
-		</ul>
-
-		Some inline code -&nbsp;<code>console.log</code>. Description Lists
-
-		<h3>Description List (h3)</h3>
-
-		They can be great for things like FAQs.
-
-		<dl>
-			<dt>Why do you never see elephants hiding in trees?</dt>
-			<dd>
-				Because they're so good at it. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
-				cupiditate laboriosam fugiat.
-			</dd>
-			<dt>What do you call someone with no body and no nose?</dt>
-			<dd>
-				Nobody knows. Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, voluptas ipsa
-				quia excepturi, quibusdam natus exercitationem sapiente tempore labore voluptatem.
-			</dd>
-		</dl>
-
-		<h3>Some Links:</h3>
-		<li>
-			<a href="https://sahilrajput.com" target="_blank"> https://sahilrajput.com </a>
-		</li>
-		<li>
-			<a href="https://blog.sahilrajput.com/" target="_blank"> https://blog.sahilrajput.com </a>
-		</li>
-	</div>
-</div>
-
-<div class="my-5 border-2 border-purple-600 p-3">
-	<h2 class="mb-8 text-2xl font-bold">Forms Example</h2>
-</div>
+<h2 class="mt-[100px] text-8xl font-bold">Tailwindcss Forms</h2>
+<Forms1 />
+<Forms2 />
---
+++ tailwind-plugins-demo/src/routes/Typography.svelte
@@ -0,0 +1,57 @@
+<!-- / // & Adding class `prose` to a html element applies tailwin-typography plugin to vanilla html inside it -->
+<div class="prose">
+	<h1>Garlic bread with cheese: What the science tells us (h1)</h1>
+	<p>
+		For years parents have espoused the health benefits of eating garlic bread with cheese to their
+		children, with the food earning such an iconic status in our culture that kids will often dress
+		up as warm, cheesy loaf for Halloween.
+	</p>
+	<p>
+		But a recent study shows that the celebrated appetizer may be linked to a series of rabies cases
+		springing up around the country.
+	</p>
+
+	<h2>Ordered, Unordered and Description Lists (h2)</h2>
+	<p>Examples of ordered, unordered lists and description lists.</p>
+
+	<h3>Ordered List (h3)</h3>
+	<ol>
+		<li>This is list item 1</li>
+		<li>This is list item 2</li>
+		<li>This is list item 3</li>
+	</ol>
+
+	<h3>Unordered List (h3)</h3>
+	<ul>
+		<li>This is list item 1</li>
+		<li>This is list item 2</li>
+		<li>This is list item 3</li>
+	</ul>
+
+	Some inline code -&nbsp;<code>console.log</code>. Description Lists
+
+	<h3>Description List (h3)</h3>
+
+	They can be great for things like FAQs.
+
+	<dl>
+		<dt>Why do you never see elephants hiding in trees?</dt>
+		<dd>
+			Because they're so good at it. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
+			cupiditate laboriosam fugiat.
+		</dd>
+		<dt>What do you call someone with no body and no nose?</dt>
+		<dd>
+			Nobody knows. Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, voluptas ipsa
+			quia excepturi, quibusdam natus exercitationem sapiente tempore labore voluptatem.
+		</dd>
+	</dl>
+
+	<h3>Some Links:</h3>
+	<li>
+		<a href="https://sahilrajput.com" target="_blank"> https://sahilrajput.com </a>
+	</li>
+	<li>
+		<a href="https://blog.sahilrajput.com/" target="_blank"> https://blog.sahilrajput.com </a>
+	</li>
+</div>
