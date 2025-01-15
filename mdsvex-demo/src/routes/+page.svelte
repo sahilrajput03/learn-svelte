13ca96bca7eb54f735076e00f534a66cdb0ed3d7
Sahil Rajput <sahilrajput03@gmail.com>
2025-01-15 23:55:45 +0530

    Update.

--- mdsvex-demo/src/routes/+page.svelte
+++ mdsvex-demo/src/routes/+page.svelte
@@ -1,3 +1,15 @@
 <h1>mdsvex Demo</h1>
 
 Visit /page1 and /page2 routes.
+
+<p>
+  `mdsvex` is useful when you want to have markdown in your local codebase
+  files.
+</p>
+
+<p>
+  <u>
+    Note from Sahil: If you want to render some markdown from a url at runtime
+    then you can simply use some other <code>markdown-to-html</code> library instead.
+  </u>
+</p>
