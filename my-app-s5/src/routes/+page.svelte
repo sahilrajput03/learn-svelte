1dec1c1503aa98878f531dbe48093a093eb3bfe6
Sahil Rajput <sahilrajput03@gmail.com>
2025-01-23 18:13:46 +0530

    Update.

--- my-app-s5/src/routes/+page.svelte
+++ my-app-s5/src/routes/+page.svelte
@@ -85,6 +85,7 @@
 	import Personal5 from './Personal5.svelte';
 	import { copyToClipboard } from './utils';
 	import Personal6 from './Personal6.svelte';
+	import Personal7 from './Personal7.svelte';
 
 	type ComponentsItemType = {
 		id: string;
@@ -526,6 +527,12 @@
 			id: '079324bb-e2ac-4ea2-a4cf-9d4c2259631d',
 			name: 'Personal6 - Vocab Collector for Books',
 			component: Personal6,
+			svelteTutorialLink: ''
+		},
+		{
+			id: 'd3608e5b-e42b-405f-967e-db14fd77a1f5',
+			name: 'Personal7 - UPI Links Testing',
+			component: Personal7,
 			svelteTutorialLink: ''
 		},
 		{
---
+++ my-app-s5/src/routes/Personal7.svelte
@@ -0,0 +1,48 @@
+<a href="upi://pay?pa=Q333412475@ybl&pn=PhonePeMerchant&mc=0000&mode=02&purpose=00">
+	Pay Vijay (all params)
+</a>
+
+<a href="upi://pay?pa=Q333412475@ybl">Pay Vijay (2 - no params)</a>
+
+<!-- from library - "..." -->
+<a href="upi://pay?pa=prem.mohapatra@oksbi&pn=PNLBot&am=1&tn=MonthlyFees&tr=uuid">
+	pay with all params</a
+>
+
+<a href="upi://pay?pa=Q333412475@ybl&pn=PhonePeMerchant&mc=5812&tn=note&am=1&cu=INR">
+	Pay (ChatGPT example, mc=5812 (eating places & restaurants), without `tid` and `tr`)
+</a>
+
+<a
+	href="upi://pay?pa=Q333412475@ybl&pn=PhonePeMerchant&mc=5812&tid=uniqueTransactionID&tr=transactionRefId&tn=note&am=1&cu=INR"
+>
+	Pay (ChatGPT example, mc=5812 (eating places & restaurants))
+</a>
+
+<!-- 
+Params:
+- pa = payee address
+- pn = payeename
+- mc = merchant code (merchant category code)
+- ? tid = transaction id
+- ? tr = transaction reference id
+- tn = transaction note
+- am = amount
+- cu = currency
+
+-->
+
+<hr />
+
+<p>Non-merchant account -</p>
+
+<a href="upi://pay?pa=8360267243@ptyes&pn=John%20Doe&am=100&cu=INR">Pay Sahil</a>
+
+<style>
+	hr,
+	a {
+		display: block;
+		margin-top: 20px;
+		color: blue;
+	}
+</style>
