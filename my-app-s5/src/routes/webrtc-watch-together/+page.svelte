c937be6687731ec9afa458dd981c359f022a0d44
Sahil Rajput <sahilrajput03@gmail.com>
2025-02-13 01:15:27 +0530

    Update.

--- my-app-s5/src/routes/webrtc-watch-together/+page.svelte
+++ my-app-s5/src/routes/webrtc-watch-together/+page.svelte
@@ -11,18 +11,24 @@ How this works for now: Open webstie in two tabs:
 
 	TODO:
 	1. Fix the bugs while playing videos and make it robust. 
+
 	TODO:
-	2. Implement features in future:
-	I need to make call independently to multiple peers like below so that more than 2 people can watch together:
-	Check chatgpt's answer here - 
-	    peerIDs.forEach((peerId) => {
-        const call = peer.call(peerId, stream);
-        call.on('stream', (remoteStream) => {
-            addVideoStream(remoteStream);
-        });
+	2. Since we can load mp3 files in <video> tag as confirmed by chatgpt here - https://chatgpt.com/c/67acf84b-16b4-8007-8d8c-559793798643
+	thus we can implement that so that we can listen to long mp3 files as well for e.gc., long lecutres of osho listening with people.
+
+	TODO:
+	3. I would need to make call independently to multiple peers like below so that more than 2 people can also watch together.
+	The reason its not doable natively with peerjs because webrtc itself doesn't support one to many communication (check below chatgpt answer).
 	Q. Does webrtc supports one to many communication? --- https://chatgpt.com/c/67acf6a2-2a04-8007-b2e9-db5e89c3e398
-
-
+	Implementing hints from ChatGPT - https://chatgpt.com/c/67acf5d0-2478-8007-a94b-8fa1ae4d494f
+		* Related code:
+		===============
+	    peerIDs.forEach((peerId) => {
+			const call = peer.call(peerId, stream);
+			call.on('stream', (remoteStream) => {
+				addVideoStream(remoteStream);
+			});
+		}
 
 -->
 
