ace162a3bdf712e97508bb7867c0f2c7105dc692
Sahil Rajput <sahilrajput03@gmail.com>
2025-02-24 18:47:32 +0530

    Update.

--- my-app-s5/src/routes/shared.svelte.ts
+++ my-app-s5/src/routes/shared.svelte.ts
@@ -2,8 +2,7 @@
 
 import { writable } from 'svelte/store';
 
-// ! ALERT: Never reassign state (`counter`) when its defined in
-//          a module but feel free to mutate it.
+// ! ALERT: Never reassign state (`counter`) when its defined in a module but feel free to mutate it.
 export const counter = $state({
     count: 0
 });
