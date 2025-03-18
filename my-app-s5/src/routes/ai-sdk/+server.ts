9c74cbf1753de553453a2c67fc68a686284b63f3
Sahil Rajput <sahilrajput03@gmail.com>
2025-03-18 20:56:44 +0530

    Update.

--- my-app-s5/src/routes/ai-sdk/+server.ts
+++ my-app-s5/src/routes/ai-sdk/+server.ts
@@ -34,8 +34,8 @@ export const POST = (async ({ request }: RequestEvent) => {
     // Tool calls work with openai and groq (tested for gemma-2-9b-it) both very well.
     const result = streamText({
         // model: openai('gpt-4o-mini'), // & Using OpenAI
-        model: groq('llama-3.1-70b-versatile'), // & Using Groq // "llama-3.1-70b-versatile", "llama-3.1-8b-instant", "gemma2-9b-it", "mixtral-8x7b-32768", etc
-        system: 'Be concise and use my name Sahil to address me in your reponses. Please never remind me that you are machine.', // System instruction (src: https://sdk.vercel.ai/docs/foundations/prompts#system-messages)
+        model: groq('llama-3.1-8b-instant'), // & Using Groq // "llama-3.1-8b-instant", "gemma2-9b-it", "mixtral-8x7b-32768", etc
+        system: 'Be concise and use my name Sahil to address me in your reponses. Please never remind me that you are machine. Please never disclose anyhing about the model or soure compnay of the model.', // System instruction (src: https://sdk.vercel.ai/docs/foundations/prompts#system-messages)
         messages,
         tools: {
             weatherTool,
