307e03dd649c2ccad24458c9ebcd9eaf93de2c58
Sahil Rajput <sahilrajput03@gmail.com>
2025-03-20 22:39:32 +0530

    Update.

--- my-app-s5/src/routes/ai-sdk/+page.svelte
+++ my-app-s5/src/routes/ai-sdk/+page.svelte
@@ -198,6 +198,7 @@
 
 		<div class="fixed right-[10px] top-[50vh] z-20">
 			<button
+				transition:fade
 				class="rounded bg-blue-500 px-4 py-2 font-bold text-white transition duration-300 ease-in-out hover:bg-blue-600"
 				onclick={stt}
 			>
