5ea0154415d49f0aef4e9b057239e524a6b8386a
Sahil Rajput <sahilrajput03@gmail.com>
2025-04-21 23:19:27 +0530

    Add deps and example code. Code works [TESTED].

--- new-ai-sdk/src/routes/+page.svelte
+++ new-ai-sdk/src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import { Chat } from '@ai-sdk/svelte';
+
+	const chat = new Chat();
+</script>
+
+<main>
+	<ul>
+		{#each chat.messages as message, messageIndex (messageIndex)}
+			<li>
+				<div>{message.role}</div>
+				<div>
+					{#each message.parts as part, partIndex (partIndex)}
+						{#if part.type === 'text'}
+							<div>{part.text}</div>
+						{/if}
+					{/each}
+				</div>
+			</li>
+		{/each}
+	</ul>
+	<form onsubmit={chat.handleSubmit}>
+		<input bind:value={chat.input} />
+		<button type="submit">Send</button>
+	</form>
+</main>
---
+++ new-ai-sdk/src/routes/api/chat/+server.ts
@@ -0,0 +1,27 @@
+import { createOpenAI } from '@ai-sdk/openai';
+import { streamText } from 'ai';
+import { createGroq } from '@ai-sdk/groq';
+import { env } from '$env/dynamic/private';
+
+// import { OPENAI_API_KEY } from '$env/static/private';
+
+// const openai = createOpenAI({
+//     apiKey: OPENAI_API_KEY,
+// });
+
+// GROQ: https://sdk.vercel.ai/providers/ai-sdk-providers/groq
+export const _groq = createGroq({
+    apiKey: env.GROQ_API_KEY ?? ""
+});
+
+export async function POST({ request }) {
+    const { messages } = await request.json();
+
+    const result = streamText({
+        // model: openai('gpt-4o'),
+        model: _groq('gemma2-9b-it'), // & Using Groq, Models: "llama-3.1-8b-instant", "gemma2-9b-it", "mixtral-8x7b-32768", etc
+        messages,
+    });
+
+    return result.toDataStreamResponse();
+}
