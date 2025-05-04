6baa28543743565d430f93e2d27c1ac2c5e19f48
Sahil Rajput <sahilrajput03@gmail.com>
2025-05-04 20:34:43 +0530

    Update.

--- new-ai-sdk/vite.config.ts
+++ new-ai-sdk/vite.config.ts
@@ -31,13 +31,12 @@ export default defineConfig({
 		]
 	},
 	// Necessary otherwise we get below message on terminal:
-	// 7:28:29 PM [vite-plugin-svelte] src/routes/+layout.svelte:17:1 No scopable elements found in template. If you're using global styles in the style tag, you should move it into an external stylesheet file and import it in JS. See https://github.com/sveltejs/vite-plugin-svelte/blob/main/docs/faq.md#where-should-i-put-my-global-styles.
-	// 7:28:31 PM [vite-plugin-svelte] src/routes/+layout.svelte:17:1 No scopable elements found in template. If you're using global styles in the style tag, you should move it into an external stylesheet file and import it in JS. See https://github.com/sveltejs/vite-plugin-svelte/blob/main/docs/faq.md#where-should-i-put-my-global-styles.
-	// 7:28:31 PM [vite] (client) ✨ new dependencies optimized: clsx, @ai-sdk/ui-utils, @ai-sdk/provider-utils, zod
-	// 7:28:31 PM [vite] (client) ✨ optimized dependencies changed. reloading
-	// The file does not exist at "/Users/apple/Documents/github_repos/learn-svelte/new-ai-sdk/node_modules/.vite/deps/chunk-2IFVHVDX.js?v=b45d3cec" which is in the optimize deps directory. The dependency might be incompatible with the dep optimizer. Try adding it to `optimizeDeps.exclude`.
-	// The file does not exist at "/Users/apple/Documents/github_repos/learn-svelte/new-ai-sdk/node_modules/.vite/deps/chunk-YJCN6N7D.js?v=b45d3cec" which is in the optimize deps directory. The dependency might be incompatible with the dep optimizer. Try adding it to `optimizeDeps.exclude`.
-	// 
+	// 		7:28:29 PM [vite-plugin-svelte] src/routes/+layout.svelte:17:1 No scopable elements found in template. If you're using global styles in the style tag, you should move it into an external stylesheet file and import it in JS. See https://github.com/sveltejs/vite-plugin-svelte/blob/main/docs/faq.md#where-should-i-put-my-global-styles.
+	// 		7:28:31 PM [vite-plugin-svelte] src/routes/+layout.svelte:17:1 No scopable elements found in template. If you're using global styles in the style tag, you should move it into an external stylesheet file and import it in JS. See https://github.com/sveltejs/vite-plugin-svelte/blob/main/docs/faq.md#where-should-i-put-my-global-styles.
+	// 		7:28:31 PM [vite] (client) ✨ new dependencies optimized: clsx, @ai-sdk/ui-utils, @ai-sdk/provider-utils, zod
+	// 		7:28:31 PM [vite] (client) ✨ optimized dependencies changed. reloading
+	// 		The file does not exist at "/Users/apple/Documents/github_repos/learn-svelte/new-ai-sdk/node_modules/.vite/deps/chunk-2IFVHVDX.js?v=b45d3cec" which is in the optimize deps directory. The dependency might be incompatible with the dep optimizer. Try adding it to `optimizeDeps.exclude`.
+	// 		The file does not exist at "/Users/apple/Documents/github_repos/learn-svelte/new-ai-sdk/node_modules/.vite/deps/chunk-YJCN6N7D.js?v=b45d3cec" which is in the optimize deps directory. The dependency might be incompatible with the dep optimizer. Try adding it to `optimizeDeps.exclude`.
 	optimizeDeps: {
 		exclude: [
 			'lightningcss.android-arm64.node'
