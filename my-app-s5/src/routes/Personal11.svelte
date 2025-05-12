156856be9230a9a392cd6813fb88667946a3db95
Sahil Rajput <sahilrajput03@gmail.com>
2025-05-13 02:21:22 +0530

    Update.

--- my-app-s5/src/routes/Personal11.svelte
+++ my-app-s5/src/routes/Personal11.svelte
@@ -23,6 +23,7 @@
 	frameborder="0"
 	allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
 	referrerpolicy="strict-origin-when-cross-origin"
+	allowfullscreen
 ></iframe>
 
 <style>
