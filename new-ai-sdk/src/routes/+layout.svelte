3df3622a91111843537ae259495974a72d0fed85
Sahil Rajput <sahilrajput03@gmail.com>
2026-03-28 00:50:06 +0530

    Update.

--- new-ai-sdk/src/routes/+layout.svelte
+++ new-ai-sdk/src/routes/+layout.svelte
@@ -9,9 +9,5 @@
 <style>
 	/* Sahil: `:root` is an alias for `html` */
 	:root {
-		/* We must set bottom margin AND padding to 0px on html element
-		otherwise it causes issues with the chat input box  while typing
-		on mobile android (TESTED). */
-		margin: 0px 20px;
 	}
 </style>
