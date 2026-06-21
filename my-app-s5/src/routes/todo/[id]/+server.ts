72631f8a04056bf1e38e0661a1bfb73dbbd43dab
Sahil Rajput <sahilrajput03@gmail.com>
2026-06-21 17:35:25 +0530

    Update.

--- my-app-s5/src/routes/todo/[id]/+server.ts
+++ my-app-s5/src/routes/todo/[id]/+server.ts
@@ -1,6 +1,6 @@
 import * as database from '$lib/server/database.js';
 
-type Type1 = { params: any, request: any, cookies: any }
+type Type1 = { params, request, cookies }
 export async function PUT({ params, request, cookies }: Type1) {
     const { done } = await request.json();
     const userid = cookies.get('userid');
@@ -9,7 +9,7 @@ export async function PUT({ params, request, cookies }: Type1) {
     return new Response(null, { status: 204 });
 }
 
-type Type2 = { params: any, request: any, cookies: any }
+type Type2 = { params, request, cookies }
 export async function DELETE({ params, cookies }: Type2) {
     const userid = cookies.get('userid');
 
