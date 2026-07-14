993b7f8792739982894ae7aa1c043a5f9acfcc96
Sahil Rajput <sahilrajput03@gmail.com>
2026-07-14 15:58:08 +0530

    Refactor code to fetch and display code snippets using Shiki for syntax highlighting

--- my-app-s5/src/routes/codexp/+page.svelte
+++ my-app-s5/src/routes/codexp/+page.svelte
@@ -1,14 +1,22 @@
 <script lang="ts">
+	import { shikiThemes } from '$lib/shiki-utils';
 	import axios from 'axios';
+	import { codeToHtml } from 'shiki';
 	import { onMount } from 'svelte';
+
+	let html = $state('');
 
 	onMount(() => {
 		async function main() {
 			const { data } = await axios.get('/codexp');
 			console.log('🚀 ~ data?', data);
+
+			// shiki (Refer file `Personal16.svelte`)
+			const lang = 'ts'; // Eg., 'js', 'svelte', etc.
+			html = await codeToHtml(data.code, { lang: lang, theme: 'dark-plus' });
 		}
 		main();
 	});
 </script>
 
-Hello world 1.
+{@html html}
--- my-app-s5/src/routes/codexp/+server.ts
+++ my-app-s5/src/routes/codexp/+server.ts
@@ -1,9 +1,12 @@
 import { json } from "@sveltejs/kit"
+import fs from 'fs'
 
 const sampleFilePath = '/Users/apple/Documents/test/qr-solution/qr-solution-backend/routes/whatsAppRouter.ts'
+
+const readFile = (filePath) => fs.readFileSync(filePath, 'utf8');
 
 export function GET() {
     // const search =  url.searchParams.get('filePath');
     // return json({ filePath });
-    return json({ message: 'Hello world 2' })
+    return json({ code: readFile(sampleFilePath) })
 }
