fc11ad955bb1439744053b6cc41cf6c31f23b7dd
Sahil Rajput <sahilrajput03@gmail.com>
2026-07-14 15:29:34 +0530

    Update.

--- my-app-s5/src/routes/links/+page.svelte
+++ my-app-s5/src/routes/links/+page.svelte
@@ -1,19 +1,30 @@
-<a class="text-blue-600 underline" href="/group101"
-	>Go to Group 101 - Setting headers as `'Content-Type': 'text/plain'``</a
->
-<a class="text-blue-600 underline" href="/group102">Go to Group 102 - Using cookies (set and get)</a
->
-<a class="text-blue-600 underline" href="/group103"
-	>Go to Group 103 - Using `$lib` alias to import form `$lib` folder</a
->
-<a class="text-blue-600 underline" href="/group104-forms">Go to Group 104 - TODO: Forms</a>
-<a class="text-blue-600 underline" href="/group105"
-	>Go to Group 105 - Make API Call - GET /roll (Code in `/routes/roll/+server.ts`)</a
->
-<a class="text-blue-600 underline" href="/group106-todo-app">Go to Group 106 - Todo app</a>
+<div class="links">
+	<a class="text-blue-600 underline" href="/group101"
+		>Go to Group 101 - Setting headers as `'Content-Type': 'text/plain'``</a
+	>
+	<a class="text-blue-600 underline" href="/group102">Go to Group 102 - Using cookies (set and get)</a
+	>
+	<a class="text-blue-600 underline" href="/group103"
+		>Go to Group 103 - Using `$lib` alias to import form `$lib` folder</a
+	>
+	<a class="text-blue-600 underline" href="/group104-forms">Go to Group 104 - TODO: Forms</a>
+	<a class="text-blue-600 underline" href="/group105"
+		>Go to Group 105 - Make API Call - GET /roll (Code in `/routes/roll/+server.ts`)</a
+	>
+	<a class="text-blue-600 underline" href="/group106-todo-app">Go to Group 106 - Todo app</a>
+</div>
 
 <style>
-	a {
+	.links {
+		counter-reset: item;
+	}
+
+	.links a {
 		display: block;
+		counter-increment: item;
+	}
+
+	.links a::before {
+		content: counter(item) '. ';
 	}
 </style>
