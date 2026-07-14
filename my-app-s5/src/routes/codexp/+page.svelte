32b481f0bb4938c39ea12a4678a50439e7f39e1e
Sahil Rajput <sahilrajput03@gmail.com>
2026-07-14 15:45:13 +0530

    Update.

--- my-app-s5/src/routes/codexp/+page.svelte
+++ my-app-s5/src/routes/codexp/+page.svelte
@@ -2,8 +2,6 @@
 	import axios from 'axios';
 	import { onMount } from 'svelte';
 
-	// TODO: Create a link to be able to browse this page from UI - http://localhost:5173/codexp
-
 	onMount(() => {
 		async function main() {
 			const { data } = await axios.get('/codexp');
