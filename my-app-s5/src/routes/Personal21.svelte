28088f693b2c3c27e1d71f46a0e1254ff586d5d6
Sahil Rajput <sahilrajput03@gmail.com>
2026-08-05 12:08:58 +0530

    Update.

---
+++ my-app-s5/src/routes/Personal21.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	// ! Unable to install wordgard because it requires node version >22.
+	// import { Wordgard, menuBar } from 'wordgard/editor';
+	// import { fullSchema } from 'wordgard/schema';
+	// import { history } from 'wordgard/history';
+	// import { onMount } from 'svelte';
+
+	// onMount(() => {
+	// 	window.wg = Wordgard.create({
+	// 		parent: document.body,
+	// 		doc: `<p>Demo content</p>`,
+	// 		config: [fullSchema(), history(), menuBar()],
+	// 	});
+	// });
+
+	// Reserved for wordgard:
+	// TODO: https://wordgard.net/examples/basic/
+	// https://wordgard.net/try/
+	// On HackerNews: https://news.ycombinator.com/item?id=48772573
+</script>
+
+<div></div>
+
+<style></style>
